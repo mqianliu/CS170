bfcb86e6303e79658a4486a7094f5b0f0f66cf8a
Jiayi Zhu <yueyue200830@berkeley.edu>
2019-04-28 13:57:35 -0700

    test2

--- solver.py
+++ solver.py
@@ -11,7 +11,6 @@ def solve(client):
     client.scout(random.choice(non_home), all_students)
     for _ in range(100):
         u, v = random.choice(list(client.G.edges()))
-        client.remote(u, v)
+        #client.remote(u, v)
 
     client.end()
-
