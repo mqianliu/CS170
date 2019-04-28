433adeb55b42d82a544891515ae3076fdbfd4d80
mqianliu <minqianliu@berkeley.edu>
2019-04-28 13:33:47 -0700

    test

--- solver.py
+++ solver.py
@@ -1,4 +1,17 @@
 # Put your solution here.
+import networkx as nx
+import random
+
 
 def solve(client):
-    pass
+    client.end()
+    client.start()
+    all_students = list(range(1, client.students + 1))
+    non_home = list(range(1, client.home)) + list(range(client.home + 1, client.v + 1))
+    client.scout(random.choice(non_home), all_students)
+    for _ in range(100):
+        u, v = random.choice(list(client.G.edges()))
+        client.remote(u, v)
+
+    client.end()
+
