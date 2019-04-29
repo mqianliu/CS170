e51e30a71107040b9ffe0a2a66603246f0d13693
Jiayi Zhu <yueyue200830@berkeley.edu>
2019-04-28 17:33:23 -0700

    some changes

--- solver.py
+++ solver.py
@@ -10,38 +10,45 @@ def solve(client):
     non_home = list(range(1, client.home)) + list(range(client.home + 1, client.v + 1))
 
 
-
-    mst_edges = [[0 for col in range(client.v + 1)] for row in range(client.v + 1)]
-    nodes = [0 for _ in range(client.v + 1)]
-    edges = []
-    for i in range(1, client.v + 1):
-        if i != client.home:
-            edges.append([client.home, i, client.G[client.home][i]['weight']])
-    for num in range(client.v):
-        edges.sort(key = lambda x : x[2])
-        while True:
-            new_edge = edges[0]
-            if nodes[new_edge[1]] == 0:
-                break
-            edges.pop(0)
-        nodes[new_edge[1]] = 1
-        mst_edges[new_edge[0]][new_edge[1]] = 1
-        mst_edges[new_edge[1]][new_edge[0]] = 1
+    def MST():
+        mst_edges = [[0 for col in range(client.v + 1)] for row in range(client.v + 1)]
+        nodes = [0 for _ in range(client.v + 1)]
+        edges = []
         for i in range(1, client.v + 1):
-            if nodes[i] == 0:
-                edges.append([new_edge[1], i, client.G[new_edge[1]][i]['weight']])
-    visited = [0 for _ in range(client.v + 1)]
+            if i != client.home:
+                edges.append([client.home, i, client.G[client.home][i]['weight']])
+        for num in range(client.v):
+            edges.sort(key = lambda x : x[2])
+            while True:
+                new_edge = edges[0]
+                if nodes[new_edge[1]] == 0:
+                    break
+                edges.pop(0)
+            nodes[new_edge[1]] = 1
+            mst_edges[new_edge[0]][new_edge[1]] = 1
+            mst_edges[new_edge[1]][new_edge[0]] = 1
+            for i in range(1, client.v + 1):
+                if nodes[i] == 0:
+                    edges.append([new_edge[1], i, client.G[new_edge[1]][i]['weight']])
 
-    def dfs(node):
-        nonlocal visited
-        visited[node] = 1
-        for i in range(1, client.v + 1):
-            if not visited[i] and mst_edges[node][i]:
-                dfs(i)
-                client.remote(i, node)
+        visited = [0 for _ in range(client.v + 1)]
+        bots = [0 for _ in range(client.v + 1)]
+        num = 0
 
+        def dfs(node):
+            nonlocal visited, bots, num
+            visited[node] = 1
+            for i in range(1, client.v + 1):
+                if not visited[i] and mst_edges[node][i]:
+                    dfs(i)
+                    n = client.remote(i, node)
+                    bots[node] += n
+                    if n > bots[i]:
+                        num += n - bots[i]
+                    if num == 5:
+                        return
 
-    dfs(client.home)
-    #print(visited)
+        dfs(client.home)
 
+    MST()
     client.end()
